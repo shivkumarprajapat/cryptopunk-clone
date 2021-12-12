6b12aa318b6044a89cc5365bee06c05ba6ed12b6
shivkumarprajapat <shivkumarprajapat28@gmail.com>
2021-12-12 13:54:10 +0530

    CollectionCard Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.scss';
+import CollectionCard from './components/Collection/CollectionCard';
 import Header from './components/Header/Header';
-
+import img from './assets/5.jpg'
 function App() {
   return (
     <div className="App">
       <Header />
+      <CollectionCard
+        id={0}
+        name={'Bandana Punk'}
+        traits={[{value:7}]} 
+          image={img}
+          />
     </div>
   );
 }
---
+++ src/components/Collection/CollectionCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './collectionCard.scss'
+const CollectionCard = ({ id, name, image, traits }) => {
+    return (
+        <div className='collectionCard'>
+            <img src={image} alt="" />
+            <div className="details">
+            <div className="name">
+                {name} <div className="id"> *#{id}</div>
+            </div>
+            <div className="priceContainer">
+                <img src={require('../../assets/weth.png').default} className='wethImage' alt="weth" />
+                <div className="price">{traits[0]?.value}</div>
+            </div>
+            </div>
+        </div>
+        )
+}
+
+export default CollectionCard
